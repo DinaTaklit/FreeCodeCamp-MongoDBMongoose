73bc41b5556e243e62752a0054f44f1d54910160
Taklit Dina <ez_taklit@esi.dz>
2023-10-10 14:19:15 +0100

    feat: Create Many Records with model.create()"

--- myApp.js
+++ myApp.js
@@ -32,8 +32,13 @@ const createAndSavePerson = (done) => {
   });
 };
 
+// Create Many Records with model.create()
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
